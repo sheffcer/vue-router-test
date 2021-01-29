3b2e70abf7b1649e2ebc385f03df8348860963f1
Alex sheffcer <a.sheffcer@gmail.com>
2021-01-29 12:02:37 +0200

    modify mailbody

--- src/components/AppEmailBody.vue
+++ src/components/AppEmailBody.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h2>{{email.title}}</h2>
-    <p>{{email.body}}</p>
+    <h2>{{email[0]}}</h2>
+    <!-- <p>{{email[0].body}}</p> -->
     <button class="btn" @click="$router.push('/login')">Закрыть</button>
   </div>
 </template>
@@ -14,7 +14,8 @@ export default {
   },
   computed: {
     email () {
-      return this.emails.find(e => e.id === this.mailId)
+      // return this.emails.find(e => e.id === this.mailId)
+      return this.emails.filter(e => e.id === this.mailId)
     }
   }
 }
